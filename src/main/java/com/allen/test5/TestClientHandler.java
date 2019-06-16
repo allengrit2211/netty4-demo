c544f13c5e236f66cfb4834e70d084d4043d1d0d
Allen <zhijun0808@gmail.com>
2019-06-16 14:21:32 +0800

    多类型

--- src/main/java/com/allen/test5/TestClientHandler.java
+++ src/main/java/com/allen/test5/TestClientHandler.java
@@ -3,16 +3,18 @@ package com.allen.test5;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.util.Random;
+
 /**
  * @ClassName: TestClientHandler
  * @description:
  * @author: Allen
  * @create: 2019-06-16 13:08
  **/
-public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
 
     }
 
@@ -20,8 +22,45 @@ public class TestClientHandler extends SimpleChannelInboundHandler<MyDataInfo.Pe
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("上线操作");
-        MyDataInfo.Person person = MyDataInfo.Person.newBuilder().setName("张三").setAge(20).setAddress("北京").build();
-        ctx.channel().writeAndFlush(person);
+        int randomInt = new Random().nextInt(3);
+
+        MyDataInfo.MyMessage myMessage = null;
+
+        System.out.println("randomInt="+randomInt);
+
+
+        switch (randomInt) {
+            case 0: {
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.PersonType)
+                        .setPerson(MyDataInfo.Person.newBuilder()
+                                .setName("张三")
+                                .setAge(20)
+                                .setAddress("北京").build())
+                        .build();
+                break;
+            }
+            case 1: {
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.DogType)
+                        .setDog(MyDataInfo.Dog.newBuilder()
+                                .setName("一只狗")
+                                .setAge(2).build())
+                        .build();
+                break;
+            }
+            case 2: {
+                myMessage = MyDataInfo.MyMessage.newBuilder()
+                        .setDataType(MyDataInfo.MyMessage.DataType.CatType)
+                        .setCat(MyDataInfo.Cat.newBuilder()
+                                .setName("一只猫")
+                                .setCity("上海").build())
+                        .build();
+                break;
+            }
+        }
+
+        ctx.channel().writeAndFlush(myMessage);
     }
 
     @Override
--- src/main/java/com/allen/test5/TestServerHandler.java
+++ src/main/java/com/allen/test5/TestServerHandler.java
@@ -9,14 +9,26 @@ import io.netty.channel.SimpleChannelInboundHandler;
  * @author: Allen
  * @create: 2019-06-16 13:01
  **/
-public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.Person> {
+public class TestServerHandler extends SimpleChannelInboundHandler<MyDataInfo.MyMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.Person msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, MyDataInfo.MyMessage msg) throws Exception {
 
-        System.out.println(msg.getName());
-        System.out.println(msg.getAge());
-        System.out.println(msg.getAddress());
+        MyDataInfo.MyMessage.DataType dataType = msg.getDataType();
 
+        if (dataType == MyDataInfo.MyMessage.DataType.PersonType) {
+            MyDataInfo.Person person = msg.getPerson();
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+            System.out.println(person.getAddress());
+        } else if (dataType == MyDataInfo.MyMessage.DataType.DogType) {
+            MyDataInfo.Dog dag = msg.getDog();
+            System.out.println(dag.getName());
+            System.out.println(dag.getAge());
+        } else {
+            MyDataInfo.Cat dag = msg.getCat();
+            System.out.println(dag.getName());
+            System.out.println(dag.getCity());
+        }
     }
 }
